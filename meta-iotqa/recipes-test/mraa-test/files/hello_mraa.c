925eda9e97b21088f71201da0cada3eff887665b
Mikko Ylinen <mikko.ylinen@linux.intel.com>
2017-04-06 12:14:40 +0300

    meta-iotqa: mraa-test: do not call mraa_deinit
    mraa 1.6.0 added a destructor attribute to mraa_deinit() so
    it gets automatically called when libmraa is unloaded.
    
    Therefore, we should not call mraa_deinit in hello_mraa.c.
    Otherwise we get a double free.
    
    Signed-off-by: Mikko Ylinen <mikko.ylinen@linux.intel.com>

--- meta-iotqa/recipes-test/mraa-test/files/hello_mraa.c
+++ meta-iotqa/recipes-test/mraa-test/files/hello_mraa.c
@@ -4,7 +4,6 @@ main(int argc, char** argv)
 {
 char* board_name = mraa_get_platform_name();
 fprintf(stdout, "hello mraa\n Version: %s\n Running on %s\n", mraa_get_version(), board_name);
-mraa_deinit();
 return MRAA_SUCCESS;
 }
 
