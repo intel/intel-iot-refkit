b0b1222bc128457f8f26a24d8816df77e1ceef3c
Jair Gonzalez <jair.de.jesus.gonzalez.plascencia@linux.intel.com>
2017-04-07 16:35:45 -0500

    meta-iotqa: Clean up Bluetooth test classes
    Remove not used tags and update comments and variable names for
    added clarity.
    
    Signed-off-by: Jair Gonzalez <jair.de.jesus.gonzalez.plascencia@linux.intel.com>

--- meta-iotqa/lib/oeqa/runtime/bluetooth/comm_bt_stability.py
+++ meta-iotqa/lib/oeqa/runtime/bluetooth/comm_bt_stability.py
@@ -7,47 +7,36 @@ from oeqa.utils.helper import shell_cmd_timeout
 from oeqa.utils.helper import get_files_dir
 from oeqa.utils.decorators import tag
 
-@tag(TestType="EFT")
+
 class BTStabilityTest(oeRuntimeTest):
-    """
-    @class BTStabilityTest
-    """
+    power_cycles = 200
+
     def setUp(self):
-        ''' initialize bluetooth class
-        @fn setUp
-        @param self
-        @return
-        '''
+        """
+        Initialize bluetooth class
+        """
         self.bt = bluetooth.BTFunction(self.target)
 
-    @tag(FeatureID="IOTOS-453")
-    def test_bt_onoff_multiple_time(self):
-        '''bluetoothctl to power on/off for multiple times
-        @fn test_bt_onoff_multiple_time
-        @param self
-        @return
-        '''
-        time=200
-        for i in range(1, time):
+    def test_bt_onoff_multiple_times(self):
+        """
+        Use bluetoothctl to power on/off multiple times
+        """
+        for i in range(1, self.power_cycles):
             self.bt.ctl_power_on()
             self.bt.ctl_power_off()
             if i % 20 == 0:
-                print ("Finish %d times, successful." % i)
+                print ("Finished %d cycles successfuly." % i)
 
-    @tag(FeatureID="IOTOS-453")
-    def test_bt_visable_onoff_multiple_time(self):
-        '''bluetoothctl to turn discoverable on/off for multiple times
-        @fn test_bt_visable_onoff_multiple_time
-        @param self
-        @return
-        '''
+    def test_bt_visible_onoff_multiple_times(self):
+        """
+        Use bluetoothctl to turn discoverable on/off multiple times
+        """
         self.bt.ctl_power_on()
-        time=200
-        for i in range(1, time):
-            self.bt.ctl_visable_on()
-            self.bt.ctl_visable_off()
+        for i in range(1, self.power_cycles):
+            self.bt.ctl_visible_on()
+            self.bt.ctl_visible_off()
             if i % 20 == 0:
-                print ("Finish %d times, successful." % i)
+                print ("Finished %d cycles successfuly." % i)
 
 ##
 # @}
