f77e798fcca5a33137098a8b33521b93476fbc2f
Yeoh Ee Peng <ee.peng.yeoh@intel.com>
2017-06-18 18:37:51 -0700

    runtime/gstreamer_cli: combined test cases to ease management
    Combined simple test cases into 1 test case to simplify
    test case management.
    
    Signed-off-by: Yeoh Ee Peng <ee.peng.yeoh@intel.com>

--- meta-iotqa/lib/oeqa/runtime/multimedia/gstreamer/gstreamer_cli.py
+++ meta-iotqa/lib/oeqa/runtime/multimedia/gstreamer/gstreamer_cli.py
@@ -1,18 +1,17 @@
 from oeqa.oetest import oeRuntimeTest
 
 class GstreamerCommandlineTest(oeRuntimeTest):
-    def test_gst_inspect_cmd_enabled(self):
+    def test_gst_inspect_can_detect_existing_element(self):
         (status, output) = self.target.run('gst-inspect-1.0')
         self.assertEqual(status, 0, msg="Error messages: %s" % output)
 
-    def test_gst_inspect_cmd_can_detect_existing_element(self):
         (status, output) = self.target.run('gst-inspect-1.0 fakesrc')
         self.assertEqual(status, 0, msg="Error messages: %s" % output)
 
-    def test_gst_launch_cmd_enabled(self):
+    def test_gst_launch_can_create_video_pipeline(self):
+        (status, output) = self.target.run('gst-launch-1.0 -v fakesrc')
+        self.assertEqual(status, 0, msg="Error messages: %s" % output)
+
         (status, output) = self.target.run('gst-launch-1.0 -v fakesrc silent=false num-buffers=3 ! fakesink silent=false')
         self.assertEqual(status, 0, msg="Error messages: %s" % output)
 
-    #def test_gst_launch_cmd_can_create_pipeline_for_video(self):
-    #    (status, output) = self.target.run('gst-launch-1.0 -v videotestsrc ! videoconvert ! autovideosink')
-    #    self.assertEqual(status, 0, msg="Error messages: %s" % output)
