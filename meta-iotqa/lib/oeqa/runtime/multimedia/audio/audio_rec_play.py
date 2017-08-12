72909e59466acf234b417841891b5e8dace529cf
nvelux <nagarajagopalx.velu@intel.com>
2017-08-12 21:59:07 +0800

    Audio recoding and playback test
    Signed-off-by: nvelux <nagarajagopalx.velu@intel.com>

--- meta-iotqa/lib/oeqa/runtime/multimedia/audio/audio_rec_play.py
+++ meta-iotqa/lib/oeqa/runtime/multimedia/audio/audio_rec_play.py
@@ -1,16 +1,18 @@
+import time
 from oeqa.oetest import oeRuntimeTest
 
 
 class AlsaTest(oeRuntimeTest):
     def test_rec_play(self):
         # Recording audio
-        (status, output) = self.target.run("arecord -d 1 -f cd -D plughw:0,0 /home/rec.wav")
-        self.assertEqual(status, 0, msg="Error messages: %s" % output)
+        (status, output) = self.target.run("arecord -d 5 -f cd -D plughw:1,0 /home/rec.wav")
+	time.sleep(6)
+        self.assertEqual(status, 0, msg="Error Not recorded: %s" % output)
         (status, output) = self.target.run("ls /home/ |grep 'rec.wav'")
-        self.assertEqual(status, 0, msg="Error messages: %s" % output)
+        self.assertEqual(status, 0, msg="Error File not present: %s" % output)
         # Playing audio
-        (status, output) = self.target.run("aplay -D plughw:0,0 /home/rec.wav &")
-        self.assertEqual(status, 0, msg="Error messages: %s" % output)
+        (status, output) = self.target.run("aplay -D plughw:1,0 /home/rec.wav &")
+        self.assertEqual(status, 0, msg="Error Not playing: %s" % output)
         # Audio running states checking
         (status, output) = self.target.run("cat /proc/asound/card0/pcm0p/sub0/status |grep 'state: RUNNING'")
-        self.assertEqual(status, 0, msg="Error messages: %s" % output)
+        self.assertEqual(status, 0, msg="Error Audio not running : %s" % output)
