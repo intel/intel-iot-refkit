134497d2100a981ffe52702c19b452db44988bb6
nvelux <nagarajagopalx.velu@intel.com>
2017-08-16 03:28:16 +0800

    pulaeaudio recording and playback testing
    Signed-off-by: nvelux <nagarajagopalx.velu@intel.com>

--- meta-iotqa/lib/oeqa/runtime/multimedia/audio/pulseaudio_rec_play.py
+++ meta-iotqa/lib/oeqa/runtime/multimedia/audio/pulseaudio_rec_play.py
@@ -9,12 +9,13 @@ class PulseaudioTest(oeRuntimeTest):
         self.assertEqual(status, 0, msg="Error pulseaudio not started: %s" % output)
         # Recording audio
         (status, output) = self.target.run("parecord -r /tmp/rec.wav &")
-        time.sleep(3)
+        time.sleep(10)
         self.assertEqual(status, 0, msg="Error not recorded: %s" % output)
         # Stop pulseaudio daemon
-        (status, output) = self.target.run("pulseaudio -k")
+        (status, output) = self.target.run("killall -9 parecord")
         self.assertEqual(status, 0, msg="Error pulseaudio not stop: %s" % output)
         # start pulseaudio daemon
+        time.sleep(10)
         (status, output) = self.target.run("pulseaudio -D")
         self.assertEqual(status, 0, msg="Error pulseaudio not started: %s" % output)
         # Checking recorded file present
@@ -23,6 +24,9 @@ class PulseaudioTest(oeRuntimeTest):
         # Playing audio
         (status, output) = self.target.run("paplay /tmp/rec.wav &")
         self.assertEqual(status, 0, msg="Error not played: %s" % output)
+        # state file copying
+        (status, output) = self.target.run("pactl list sinks > /tmp/pula.txt")
+        self.assertEqual(status, 0, msg="Error not copied: %s" % output)
         # Audio running states checking
-        (status, output) = self.target.run("cat /proc/asound/card1/pcm3p/sub0/status |grep 'state: RUNNING'")
+        (status, output) = self.target.run("grep 'RUNNING' /tmp/pula.txt")
         self.assertEqual(status, 0, msg="Error not running: %s" % output)
