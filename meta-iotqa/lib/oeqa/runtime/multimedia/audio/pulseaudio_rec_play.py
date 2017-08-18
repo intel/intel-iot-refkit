b674d54b9abdd15a52653b8107361ba571c505de
nvelux <nagarajagopalx.velu@intel.com>
2017-08-18 22:51:19 +0800

    Pulseaudio testing
    Signed-off-by: nvelux <nagarajagopalx.velu@intel.com>

--- meta-iotqa/lib/oeqa/runtime/multimedia/audio/pulseaudio_rec_play.py
+++ meta-iotqa/lib/oeqa/runtime/multimedia/audio/pulseaudio_rec_play.py
@@ -8,14 +8,14 @@ class PulseaudioTest(oeRuntimeTest):
         (status, output) = self.target.run("pulseaudio -D")
         self.assertEqual(status, 0, msg="Error pulseaudio not started: %s" % output)
         # Recording audio
-        (status, output) = self.target.run("parecord -r /tmp/rec.wav &")
+        (status, output) = self.target.run("parecord -r /tmp/rec.wav&")
         time.sleep(10)
         self.assertEqual(status, 0, msg="Error not recorded: %s" % output)
         # Stop pulseaudio daemon
         (status, output) = self.target.run("killall -9 parecord")
         self.assertEqual(status, 0, msg="Error pulseaudio not stop: %s" % output)
         # Playing audio
-        (status, output) = self.target.run("paplay /tmp/rec.wav & disown")
+        (status, output) = self.target.run("paplay /tmp/rec.wav&")
         self.assertEqual(status, 0, msg="Error not played: %s" % output)
         # RUNNING state checking
         time.sleep(3)
