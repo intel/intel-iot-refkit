88274cfb445503ea38907e915b6ebbe9db0b4ab6
nvelux <nagarajagopalx.velu@intel.com>
2017-08-21 21:25:10 +0800

    Pulse audio playing debugging
    Signed-off-by: nvelux <nagarajagopalx.velu@intel.com>

--- meta-iotqa/lib/oeqa/runtime/multimedia/audio/pulseaudio_rec_play.py
+++ meta-iotqa/lib/oeqa/runtime/multimedia/audio/pulseaudio_rec_play.py
@@ -8,14 +8,21 @@ class PulseaudioTest(oeRuntimeTest):
         (status, output) = self.target.run("pulseaudio -D")
         self.assertEqual(status, 0, msg="Error pulseaudio not started: %s" % output)
         # Recording audio
-        (status, output) = self.target.run("parecord -r /tmp/rec.wav&")
+        (status, output) = self.target.run("parecord -r /tmp/rec.wav &")
         time.sleep(10)
         self.assertEqual(status, 0, msg="Error not recorded: %s" % output)
         # Stop pulseaudio daemon
-        (status, output) = self.target.run("killall -9 parecord")
-        self.assertEqual(status, 0, msg="Error pulseaudio not stop: %s" % output)
+        (status, output) = self.target.run("pulseaudio -k")
+        self.assertEqual(status, 0, msg="Error pulseaudio not stoped: %s" % output)
+        time.sleep(2)
+        # Start pulseaudio daemon
+        (status, output) = self.target.run("pulseaudio -D")
+        self.assertEqual(status, 0, msg="Error pulseaudio not started: %s" % output)
+        # Stop pulseaudio daemon
+        # (status, output) = self.target.run("killall -9 parecord")
+        # self.assertEqual(status, 0, msg="Error pulseaudio not stop: %s" % output)
         # Playing audio
-        (status, output) = self.target.run("paplay /tmp/rec.wav&")
+        (status, output) = self.target.run("paplay /tmp/rec.wav &")
         self.assertEqual(status, 0, msg="Error not played: %s" % output)
         # RUNNING state checking
         time.sleep(3)
